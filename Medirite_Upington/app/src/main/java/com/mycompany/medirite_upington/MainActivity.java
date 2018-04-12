1c5a7913536cfc1a42b91054073ecf9eb4c23c91
salazar3593 <salazar3593@gmail.com>
2018-04-12 20:48:26 +0200

    I created the project, added a button. There seems to be a bug in the Main_activity.java file. I will fix it later.

---
+++ Medirite_Upington/app/src/main/java/com/mycompany/medirite_upington/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mycompany.medirite_upington;
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		Button button=findViewById(R.id.btn1);
+    }
+}
